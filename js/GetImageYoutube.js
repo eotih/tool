10dd0992a1ad6109edc7cfe478f8dad84caa8df5
Le Trong Hieu <eotihvn@gmail.com>
2021-09-29 10:38:17 +0700

    thêm iframe

--- js/GetImageYoutube.js
+++ js/GetImageYoutube.js
@@ -3,9 +3,15 @@ function splitLink() {
     var url = dataInput.split('=');
     var resultID = url[url.length - 1];
     getImage(resultID);
+    getIframe(resultID)
 }
 function getImage(input) {
     const BASE_URL = "https://img.youtube.com/vi/"
     var result = BASE_URL + input +'/sddefault.jpg'; 
     document.getElementById("showImage").src = result;
+}
+function getIframe(input) {
+    const BASE_URL = "https://www.youtube.com/embed/"
+    var result = BASE_URL + input; 
+    document.getElementById("iframeYoutube").src = result;
 }
